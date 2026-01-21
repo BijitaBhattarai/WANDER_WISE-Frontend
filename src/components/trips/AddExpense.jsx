bdb5d220671544282b7c4e1d8894a4984385edde
Bijita Bhattarai <bhattaraibijita103@gmail.com>
2026-01-21 17:10:51 +0545

    add expense is completed

--- src/components/trips/AddExpense.jsx
+++ src/components/trips/AddExpense.jsx
@@ -8,8 +8,40 @@ import {
 } from "../ui/card";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { toast } from "sonner";
+import api from "@/api/axios";
 
-const AddExpense = ({ trip }) => {
+const AddExpense = ({ trip, setDependency }) => {
+  const onSubmit = async () => {
+    try {
+      const expenseName = document.getElementById("expense-name");
+      const expenseAmount = document.getElementById("expense-amount");
+      if (expenseName.value == "" || expenseAmount.value == "") {
+        toast.error("Please fill all the fields.");
+        return;
+      }
+      const expenseData = {
+        name: expenseName.value,
+        amount: Number(expenseAmount.value),
+      };
+      const response = await api.post(
+        `/trips/expenses/${trip._id}`,
+        expenseData,
+      );
+      console.log(response);
+      if (response.status === 200) {
+        toast.success(response.data?.message);
+        setDependency((prev) => prev + 1);
+        expenseName.value = " ";
+        expenseAmount.value = " ";
+      } else {
+        toast.error("Failed to add expense. Please try again");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to add expense. Please try again");
+    }
+  };
   return (
     <Card>
       <CardHeader>
@@ -17,8 +49,22 @@ const AddExpense = ({ trip }) => {
         <CardDescription>Add your expense amount.</CardDescription>
       </CardHeader>
       <CardContent>
-        <Input type="number" placeholder="100" className="mb-4" />
-        <Button className="w-full">Add Expense</Button>
+        <Input
+          id="expense-name"
+          type="text"
+          placeholder="Train Ticket"
+          className="mb-4"
+        />
+        <Input
+          id="expense-amount"
+          type="number"
+          placeholder="100"
+          className="mb-4"
+        />
+
+        <Button onClick={onSubmit} className="w-full">
+          Add Expense
+        </Button>
       </CardContent>
     </Card>
   );
